23bf97fa4c79b39c17341201c38645799dc72fdf
Krishaay Jois <krishaayjois@gmail.com>
2024-05-16 12:53:09 +0530

    added navigation links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,9 @@
     import "../app.css"
     import {ToastContainer,BootstrapToast} from "svelte-toasts";
     import {auth,db,storage } from '$lib/firebase';
-    import {FirebaseApp, SignedIn} from "sveltefire";
-    import {ArrowUpRight} from "lucide-svelte";
+    import {FirebaseApp, } from "sveltefire";
+    import {ArrowUpRight,Disc} from "lucide-svelte";
+    import {page} from '$app/stores';
     export let data;
 </script>
 
@@ -12,6 +13,11 @@
         <BootstrapToast {data} />
 </ToastContainer>
 
-
+    {#if ["/","/leaderboard","/team"].includes($page.url.pathname)}
+        <div class="navbar">
+            <a class="btn btn-ghost text-md text-primary" href="/leaderboard"><ArrowUpRight/> leaderboard</a>
+            <a class="btn btn-ghost text-md" href="/play" disabled><Disc /> play</a>
+        </div>
+        {/if}
 <slot />
 </FirebaseApp>
