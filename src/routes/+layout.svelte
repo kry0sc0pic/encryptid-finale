d9179f9789b9ffaccde28902e6bd49e79d053481
Krishaay Jois <krishaayjois@gmail.com>
2024-05-17 14:52:00 +0530

    enabled play button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,7 +18,7 @@
             <a class="btn btn-ghost text-md" class:text-primary={$page.url.pathname==="/"} href="/"><ArrowUpRight/> home</a>
             <a class="btn btn-ghost text-md" class:text-primary={$page.url.pathname==="/leaderboard"} href="/leaderboard"><ArrowUpRight/> leaderboard</a>
             {#if ![undefined,null].includes(data.userTeam)}<a class="btn btn-ghost text-md" class:text-primary={$page.url.pathname==="/team"} href="/team"><ArrowUpRight/> team</a>{/if}
-            {#if data.banned === false && ![undefined,null].includes(data.userTeam)}<a class="btn btn-ghost text-md" href="/play" disabled><Disc /> play</a>{/if}
+            {#if data.banned === false && ![undefined,null].includes(data.userTeam)}<a class="btn btn-ghost text-md" href="/play"><Disc /> play</a>{/if}
         </div>
         {/if}
 <slot />
